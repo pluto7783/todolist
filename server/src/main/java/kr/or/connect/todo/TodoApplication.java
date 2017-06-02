b1ef27545d11f3edb897cd5c08031e72965a5f29
kim <karolus@naver.com>
2017-06-02 09:48:34 +0900

    (김종철) todolist 과제

--- server/src/main/java/kr/or/connect/todo/TodoApplication.java
+++ server/src/main/java/kr/or/connect/todo/TodoApplication.java
@@ -1,22 +1,12 @@
 package kr.or.connect.todo;
 
-import org.h2.server.web.WebServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class TodoApplication extends WebMvcConfigurerAdapter {
 	public static void main(String[] args) {
 		SpringApplication.run(TodoApplication.class, args);
-	}
-	
-	@Bean
-	public ServletRegistrationBean h2servletRegistration() {
-	    ServletRegistrationBean registration = new ServletRegistrationBean(new WebServlet());
-	    registration.addUrlMappings("/console/*");
-	    return registration;
 	}
 }
